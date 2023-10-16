80cbae42727329dae1b3c8c48cb64579e76116cd
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-16 00:17:42 -0600

    Script

---
+++ Script de la BD.sql
@@ -0,0 +1,19 @@
+USE [Programacion2]
+GO
+/****** Object:  Table [dbo].[Estudiante]   ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Estudiante](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombres] [varchar](255) NOT NULL,
+	[Apellidos] [varchar](255) NULL,
+	[Edad] [int] NOT NULL,
+	[Sexo] [varchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
